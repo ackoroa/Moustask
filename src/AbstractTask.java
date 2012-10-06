bd299a09b0f8c51005121405592ce7ecf14df32e
Loy Yusong@LoyYusong-PC.comp.nus.edu.sg <Loy Yusong@LoyYusong-PC.comp.nus.edu.sg>
2012-10-06 11:06:00 +0800

    Added status for task

--- src/AbstractTask.java
+++ src/AbstractTask.java
@@ -2,21 +2,28 @@ public abstract class AbstractTask {
     public final static int TIMED = 0;
     public final static int FLOAT = 1;
     public final static int DEADLINE = 2;
+    
+    public final static int UNDONE = 0;
+    public final static int DONE = 1;
+    public final static int IMPOSSIBLE = 2;
 
     private String description;
     private String venue;
     private int type;
+    private int status;
     
     public AbstractTask(String desc, int t) {
 	description = desc;
 	type = t;
 	venue = "";
+	status = UNDONE;
     }
 
     public AbstractTask(String desc, String v, int t) {
 	description = desc;
 	type = t;
 	venue = v;
+	status = UNDONE;
     }
 
     public String getDescription() {
@@ -39,4 +46,11 @@ public abstract class AbstractTask {
 	return type;
     }
     
+    public void setStatus(int s){
+    	status = s;
+    }
+    
+    public int getStatus(){
+    	return status;
+    }
 }
