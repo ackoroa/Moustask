9e158927b24c50b0e506b47cc704923d75a93605
ChingSK@lenovo-y450.gateway.2wire.net <ChingSK@lenovo-y450.gateway.2wire.net>
2012-10-06 23:04:34 +0800

    CLI to print Display command

--- src/CLI.java
+++ src/CLI.java
@@ -19,9 +19,54 @@ class CLI {
 
 	private void printMessage() {
 		if (taskResult.getType() == taskResult.type.ADD) {
-			System.out.println("Task " + taskResult.getTasks().get(0).getDescription() + " has been add.");
+			System.out.println("Task "
+					+ taskResult.getTasks().get(0).getDescription()
+					+ " has been add.");
+		} else if (taskResult.getType() == taskResult.type.DISPLAY) {
+			if (taskResult.getTasks().isEmpty()) {
+				System.out.println("There is no task.");
+			} else {
+				/*
+				 * SUGGESTED FORMAT: System.out.println(
+				 * "123. Online Assignment afj jsfjdjfsdjja | Venue: Orchard Road"
+				 * );
+				 * System.out.println("Categories: Floating | Status: Undone");
+				 * System
+				 * .out.println("Start: 2012-12-20 13:00 | End: 2012-12-20 13:00"
+				 * );
+				 */
+				for (int i = 0; i < taskResult.getTasks().size(); i++) {
+					if (taskResult.getTasks().get(i).getType() == 0) {
+
+					} else if (taskResult.getTasks().get(i).getType() == 1) {
+						System.out.println((i + 1) + ". "
+								+ taskResult.getTasks().get(i).getDescription()
+								+ " | Venue: "
+								+ taskResult.getTasks().get(i).getVenue());
+						System.out.println("Category: Floating | Status: "
+								+ getStatus(taskResult.getTasks().get(i)
+										.getStatus()));
+					}
+
+				}
+			}
+		} else if (taskResult.getType() == taskResult.type.UNDONULL) {
+			System.out.println("There is nothing to undo.");
 		} else if (taskResult.getType() == taskResult.type.INVALID) {
 			System.out.println("Invalid Command.");
+		}
+	}
+
+	private static String getStatus(int taskStatusNumber) {
+		switch (taskStatusNumber) {
+		case 0:
+			return "Undone";
+		case 1:
+			return "Done";
+		case 2:
+			return "Impossible";
+		default:
+			return "Unknown";
 		}
 	}
 }
