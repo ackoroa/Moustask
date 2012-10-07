2e466e4e896f3e3dd98dc52c091e56f67f74bb56
Arnold <arnold.koroa@gmail.com>
2012-10-07 23:37:47 +0800

    Added assertions and exceptions to Delete and Edit

--- src/Delete.java
+++ src/Delete.java
@@ -4,7 +4,7 @@ import java.util.Vector;
 public class Delete implements UndoableCommand {
     private List<AbstractTask> deleteSpace, wholeTaskList;
     int index;
-    AbstractTask taskToDeleted;
+    AbstractTask deletedTask;
 
     public Delete(List<AbstractTask> deleteSpace, int index) {
 	this.deleteSpace = deleteSpace;
@@ -12,21 +12,31 @@ public class Delete implements UndoableCommand {
     }
 
     public List<AbstractTask> execute(List<AbstractTask> wholeTaskList) {
-	taskToDeleted = deleteSpace.get(index);
+	if (wholeTaskList == null || wholeTaskList.size() <= 0)
+	    throw new IllegalArgumentException(
+		    "taskList cannot be empty or null");
+	if (index < 0 || index >= deleteSpace.size())
+	    throw new IndexOutOfBoundsException(
+		    "index pointer is outside the delete space");
+
+	deletedTask = deleteSpace.get(index);
 	this.wholeTaskList = wholeTaskList;
-	wholeTaskList.remove(taskToDeleted);
+	wholeTaskList.remove(deletedTask);
 
 	List<AbstractTask> returnList = new Vector<AbstractTask>();
-	returnList.add(taskToDeleted);
+	returnList.add(deletedTask);
 
 	return returnList;
     }
 
     public List<AbstractTask> undo() {
-	wholeTaskList.add(taskToDeleted);
+	assert wholeTaskList != null; // the task list from where the deleted
+				      // task came from must still exist
+
+	wholeTaskList.add(deletedTask);
 
 	List<AbstractTask> returnList = new Vector<AbstractTask>();
-	returnList.add(taskToDeleted);
+	returnList.add(deletedTask);
 
 	return returnList;
     }
--- src/Edit.java
+++ src/Edit.java
@@ -1,24 +1,47 @@
 import java.util.List;
+import java.util.StringTokenizer;
+import java.util.Vector;
 
 public class Edit implements UndoableCommand {
     private List<AbstractTask> editSpace, wholeTaskList;
+    private AbstractTask editedTask;
+    private String editParameter;
     int index;
 
     public Edit(List<AbstractTask> editSpace, int index, String editParameter) {
 	this.editSpace = editSpace;
 	this.index = index;
+	this.editParameter = editParameter;
     }
 
-    @Override
     public List<AbstractTask> execute(List<AbstractTask> wholeTaskList) {
+	if (wholeTaskList == null || wholeTaskList.size() <= 0)
+	    throw new IllegalArgumentException(
+		    "taskList cannot be empty or null");
+	if (editParameter == null || editParameter.length() <= 0)
+	    throw new IllegalArgumentException(
+		    "the edit parameter cannot be empty or null");
+	if (index < 0 || index >= editSpace.size())
+	    throw new IndexOutOfBoundsException(
+		    "index pointer is outside the edit space");
+
 	this.wholeTaskList = wholeTaskList;
-	
-	return null;
+	editedTask = editSpace.get(index);
+
+	StringTokenizer editParameterTokenizer = new StringTokenizer(
+		editParameter, ".");
+
+	List<AbstractTask> returnList = new Vector<AbstractTask>();
+	returnList.add(editedTask);
+
+	return returnList;
     }
 
-    @Override
     public List<AbstractTask> undo() {
-	// TODO Auto-generated method stub
+	assert wholeTaskList != null; // the task list from where the edited
+				      // task came from must still exist
+	assert editedTask != null; // the edited task must still exist
+
 	return null;
     }
 
