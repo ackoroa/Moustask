5a643fb90a23866061b1b64880b5e694cec1af7b
ChingSK@lenovo-y450.gateway.2wire.net <ChingSK@lenovo-y450.gateway.2wire.net>
2012-10-07 15:21:45 +0800

    Completed Logic for all commands

--- src/Logic.java
+++ src/Logic.java
@@ -4,20 +4,24 @@ import java.util.Stack;
 
 public class Logic {
 	private List<AbstractTask> taskList;
+	private List<AbstractTask> searchOrDisplayTaskList;
 	private TypeTaskPair taskResult;
-	private Stack<Command> undoStack;
-	//private Storage storageObject;
+	private Stack<UndoableCommand> undoStack;
+
+	// private Storage storageObject;
 
 	public Logic() {
 		taskList = new LinkedList<AbstractTask>();
-		undoStack = new Stack<Command>();
-		//storageObject = new Storage();
+		searchOrDisplayTaskList = new LinkedList<AbstractTask>();
+		undoStack = new Stack<UndoableCommand>();
+		// storageObject = new Storage();
+		// taskList = storageObject.load();
 	}
 
 	public TypeTaskPair processCommand(String taskMessages) {
 		String commandTypeString = getFirstWord(taskMessages);
 		String commandMessage = getMessage(taskMessages);
-		checkCommandType(commandTypeString, commandMessage);
+		checkIsTaskListEmpty(commandTypeString, commandMessage);
 		return taskResult;
 	}
 
@@ -32,32 +36,119 @@ public class Logic {
 		return checkCommandType;
 	}
 
-	private void checkCommandType(String commandTypeString,
+	private void checkIsTaskListEmpty(String commandTypeString,
 			String commandMessage) {
+		if (taskList.isEmpty()) {
+			if ((commandTypeString.equalsIgnoreCase(".add"))
+					|| (commandTypeString.equalsIgnoreCase(".undo"))
+					|| (commandTypeString.equalsIgnoreCase(".help"))
+					|| (commandTypeString.equalsIgnoreCase(".exit"))) {
+				executeCommand(commandTypeString, commandMessage);
+			} else if ((commandTypeString.equalsIgnoreCase(".delete"))
+					|| (commandTypeString.equalsIgnoreCase(".edit"))
+					|| (commandTypeString.equalsIgnoreCase(".search"))
+					|| (commandTypeString.equalsIgnoreCase(".display"))
+					|| (commandTypeString.equalsIgnoreCase(".clear"))) {
+				taskResult = new TypeTaskPair(TypeTaskPair.Type.EMPTY, null);
+			}
+		} else if (!taskList.isEmpty()) {
+			checkIsSearchOrDisplayTaskListEmpty(commandTypeString,
+					commandMessage);
+		}
+	}
+
+	private void checkIsSearchOrDisplayTaskListEmpty(String commandTypeString,
+			String commandMessage) {
+		if (searchOrDisplayTaskList.isEmpty()) {
+			if ((commandTypeString.equalsIgnoreCase(".edit"))
+					|| (commandTypeString.equalsIgnoreCase(".delete"))) {
+				taskResult = new TypeTaskPair(TypeTaskPair.Type.ERROR, null);
+			} else {
+				executeCommand(commandTypeString, commandMessage);
+			}
+		} else if (!searchOrDisplayTaskList.isEmpty()) {
+			if ((commandTypeString.equalsIgnoreCase(".add"))
+					|| (commandTypeString.equalsIgnoreCase(".search"))
+					|| (commandTypeString.equalsIgnoreCase(".display"))
+					|| (commandTypeString.equalsIgnoreCase(".clear"))
+					|| (commandTypeString.equalsIgnoreCase(".undo"))
+					|| (commandTypeString.equalsIgnoreCase(".help"))
+					|| (commandTypeString.equalsIgnoreCase(".exit"))) {
+				searchOrDisplayTaskList = new LinkedList<AbstractTask>();
+			}
+			executeCommand(commandTypeString, commandMessage);
+		}
+	}
+
+	private void executeCommand(String commandTypeString, String commandMessage) {
 		if (commandTypeString.equalsIgnoreCase(".add")) {
 			Add addObject = new Add(commandMessage);
 			List<AbstractTask> addResult = addObject.execute(taskList);
 			undoStack.push(addObject);
 			taskResult = new TypeTaskPair(TypeTaskPair.Type.ADD, addResult);
 		} else if (commandTypeString.equalsIgnoreCase(".delete")) {
-
+			try {
+				int index = Integer.parseInt(commandMessage);
+				Delete deleteObject = new Delete(searchOrDisplayTaskList, index);
+				List<AbstractTask> deleteResult = deleteObject
+						.execute(taskList);
+				taskResult = new TypeTaskPair(TypeTaskPair.Type.DELETE,
+						deleteResult);
+			} catch (NumberFormatException e) {
+				taskResult = new TypeTaskPair(null, null);
+				System.out.println("Error - " + e.getMessage());
+			} catch (IndexOutOfBoundsException e) {
+				taskResult = new TypeTaskPair(null, null);
+				System.out.println("Error - " + e.getMessage());
+			}
 		} else if (commandTypeString.equalsIgnoreCase(".edit")) {
-
+			try {
+				int index = Integer.parseInt(commandMessage);
+				Edit editObject = new Edit(searchOrDisplayTaskList, index);
+				List<AbstractTask> editResult = editObject.execute(taskList);
+				taskResult = new TypeTaskPair(TypeTaskPair.Type.DELETE,
+						editResult);
+			} catch (NumberFormatException e) {
+				taskResult = new TypeTaskPair(null, null);
+				System.out.println("Error - " + e.getMessage());
+			} catch (IndexOutOfBoundsException e) {
+				taskResult = new TypeTaskPair(null, null);
+				System.out.println("Error - " + e.getMessage());
+			}
 		} else if (commandTypeString.equalsIgnoreCase(".search")) {
-
-		} else if (commandTypeString.equalsIgnoreCase(".clear")) {
-
+			// Search searchObject = new Search();
+			// searchOrDisplayTaskList = searchObject.execute(commandMessage);
+			taskResult = new TypeTaskPair(TypeTaskPair.Type.SEARCH,
+					searchOrDisplayTaskList);
 		} else if (commandTypeString.equalsIgnoreCase(".display")) {
+			searchOrDisplayTaskList = taskList;
 			taskResult = new TypeTaskPair(TypeTaskPair.Type.DISPLAY, taskList);
-		}else if (commandTypeString.equalsIgnoreCase(".undo")) {
-			if(undoStack.isEmpty()) {
+		} else if (commandTypeString.equalsIgnoreCase(".undo")) {
+			if (undoStack.isEmpty()) {
 				taskResult = new TypeTaskPair(TypeTaskPair.Type.UNDONULL, null);
 			} else {
-				//taskResult = new TypeTaskPair(TypeTaskPair.Type., taskList);
+				UndoableCommand undoableCommandObject = undoStack.peek();
+				undoStack.pop().undo();
+				if (undoableCommandObject instanceof Add) {
+					taskResult = new TypeTaskPair(TypeTaskPair.Type.UNDOADD,
+							null);
+				} else if (undoableCommandObject instanceof Delete) {
+					taskResult = new TypeTaskPair(TypeTaskPair.Type.UNDODELETE,
+							null);
+				} else if (undoableCommandObject instanceof Edit) {
+					taskResult = new TypeTaskPair(TypeTaskPair.Type.UNDOEDIT,
+							null);
+				}/*
+				 * else if (undoableCommandObject instanceof Clear) { taskResult
+				 * = new TypeTaskPair(TypeTaskPair.Type.UNDOCLEAR, null); }
+				 */
 			}
-		}
-		else if (commandTypeString.equalsIgnoreCase(".help")) {
-
+		} else if (commandTypeString.equalsIgnoreCase(".clear")) {
+			// Clear clearObject = new Clear(taskList);
+			// clearObject.execute();
+			taskResult = new TypeTaskPair(TypeTaskPair.Type.CLEAR, null);
+		} else if (commandTypeString.equalsIgnoreCase(".help")) {
+			taskResult = new TypeTaskPair(TypeTaskPair.Type.HELP, null);
 		} else if (commandTypeString.equalsIgnoreCase(".exit")) {
 			System.exit(0);
 		} else {
@@ -66,11 +157,10 @@ public class Logic {
 
 		// DEBUG: TASK LIST MEMORY VIEW
 		/**
-		 System.out.println("TASK LIST MEMORY VIEW: ");
-		 for (int i = 0; i < taskList.size(); i++) {
-		 	System.out.println(taskList.get(i).getDescription());
-		 }
-		 System.out.println("// END OF TASK LIST MEMORY VIEW //");
+		 * System.out.println("TASK LIST MEMORY VIEW: "); for (int i = 0; i <
+		 * taskList.size(); i++) {
+		 * System.out.println(taskList.get(i).getDescription()); }
+		 * System.out.println("// END OF TASK LIST MEMORY VIEW //");
 		 */
 	}
 }
