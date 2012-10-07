c1dde9f3712bbd22b60e9138f3e039152431bd4b
ChingSK@lenovo-y450.gateway.2wire.net <ChingSK@lenovo-y450.gateway.2wire.net>
2012-10-07 15:20:50 +0800

    More enum type

--- src/TypeTaskPair.java
+++ src/TypeTaskPair.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class TypeTaskPair {
 	public static enum Type {
 		// list of possible operation types
-		ADD, DELETE, EDIT, CLEAR, SEARCH, DISPLAY, INVALID, ERROR, UNDOADD, UNDODELETE, UNDOEDIT, UNDOCLEAR, UNDONULL, HELP
+		ADD, DELETE, EDIT, CLEAR, SEARCH, DISPLAY, INVALID, EMPTY, ERROR, UNDOADD, UNDODELETE, UNDOEDIT, UNDOCLEAR, UNDONULL, HELP
 	}
 
 	Type type;
