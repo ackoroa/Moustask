5749598f5efce33d4d80f4164fcc23d050c3d870
Arnold <arnold.koroa@gmail.com>
2012-10-09 20:53:47 +0800

    Added clone to AbstractTask for copying Taks to another object

--- src/AbstractTask.java
+++ src/AbstractTask.java
@@ -2,7 +2,7 @@ public abstract class AbstractTask {
     public final static String TIMED = "TIMED";
     public final static String FLOAT = "FLOAT";
     public final static String DEADLINE = "DEADLINE";
-    
+
     public final static int UNDONE = 0;
     public final static int DONE = 1;
     public final static int IMPOSSIBLE = 2;
@@ -11,19 +11,29 @@ public abstract class AbstractTask {
     private String venue;
     private String type;
     private int status;
-    
+
     public AbstractTask(String desc, String t) {
 	description = desc;
 	type = t;
 	venue = "";
 	status = UNDONE;
     }
-    
+
     public AbstractTask(String desc, String v, String t) {
 	description = desc;
 	type = t;
 	venue = v;
 	status = UNDONE;
+    }
+
+    public Object clone() {
+	try {
+	    return super.clone();
+	} catch (CloneNotSupportedException e) {
+	    System.out.println("Cloning Task Failed");
+	    assert false;
+	}
+	return null;
     }
 
     public String getDescription() {
@@ -45,12 +55,12 @@ public abstract class AbstractTask {
     public String getType() {
 	return type;
     }
-    
-    public void setStatus(int s){
-    	status = s;
+
+    public void setStatus(int s) {
+	status = s;
     }
-    
-    public int getStatus(){
-    	return status;
+
+    public int getStatus() {
+	return status;
     }
 }
