212b5bf080a76ec5ca6441447bb8d7e39a7d847b
Arnold <arnold.koroa@gmail.com>
2012-10-09 20:53:23 +0800

    Stub for Edit's unit Test. Minor edit on Delete

--- src/Delete.java
+++ src/Delete.java
@@ -6,36 +6,42 @@ public class Delete implements UndoableCommand {
     int index;
     AbstractTask deletedTask;
 
+    // Initialize delete parameters
     public Delete(List<AbstractTask> deleteSpace, int index) {
-	this.deleteSpace = deleteSpace;
-	this.index = index;
-    }
-
-    public List<AbstractTask> execute(List<AbstractTask> wholeTaskList) {
-	if (wholeTaskList == null || wholeTaskList.size() <= 0)
-	    throw new IllegalArgumentException(
-		    "taskList cannot be empty or null");
 	if (index < 0 || index >= deleteSpace.size())
 	    throw new IndexOutOfBoundsException(
 		    "index pointer is outside the delete space");
 
-	deletedTask = deleteSpace.get(index-1);
+	this.deleteSpace = deleteSpace;
+	this.index = index;
+    }
+
+    // Deletes the specified task from taskList, stores the deletedTask and
+    // returns it in a list
+    public List<AbstractTask> execute(List<AbstractTask> wholeTaskList) {
+	if (wholeTaskList == null || wholeTaskList.size() <= 0)
+	    throw new IllegalArgumentException(
+		    "taskList cannot be empty or null");
+
+	deletedTask = deleteSpace.get(index - 1);
 	this.wholeTaskList = wholeTaskList;
-	
+
 	wholeTaskList.remove(deletedTask);
 
 	return generateReturnList();
     }
 
+    // Undoes this delete operation
+    // returns the task re-added
     public List<AbstractTask> undo() {
 	// the task list from where the deleted task came from must still exist
-	assert wholeTaskList != null; 
-	
+	assert wholeTaskList != null;
+
 	wholeTaskList.add(deletedTask);
 	return generateReturnList();
     }
 
-
+    // Creates the list of task to be returned
     private List<AbstractTask> generateReturnList() {
 	List<AbstractTask> returnList = new Vector<AbstractTask>();
 	returnList.add(deletedTask);
