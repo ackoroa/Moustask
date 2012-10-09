658403612a5c2ea53b479c463258d219562a7815
Arnold <arnold.koroa@gmail.com>
2012-10-09 20:54:11 +0800

    Implemented Edit. Only supports editing description for now.

--- src/Edit.java
+++ src/Edit.java
@@ -4,20 +4,12 @@ import java.util.Vector;
 
 public class Edit implements UndoableCommand {
     private List<AbstractTask> editSpace, wholeTaskList;
-    private AbstractTask oldTask;
+    private AbstractTask originalTask, editedTask;
     private String editParameter;
     int index;
 
+    // Initializes the edit parameters
     public Edit(List<AbstractTask> editSpace, int index, String editParameter) {
-	this.editSpace = editSpace;
-	this.index = index;
-	this.editParameter = editParameter;
-    }
-
-    public List<AbstractTask> execute(List<AbstractTask> wholeTaskList) {
-	if (wholeTaskList == null || wholeTaskList.size() <= 0)
-	    throw new IllegalArgumentException(
-		    "taskList cannot be empty or null");
 	if (editParameter == null || editParameter.length() <= 0)
 	    throw new IllegalArgumentException(
 		    "the edit parameter cannot be empty or null");
@@ -25,24 +17,60 @@ public class Edit implements UndoableCommand {
 	    throw new IndexOutOfBoundsException(
 		    "index pointer is outside the edit space");
 
+	this.editSpace = editSpace;
+	this.index = index;
+	this.editParameter = editParameter;
+    }
+
+    // Executes the edit operation, saving both the original and edited tasks
+    // and returning them in a list original->edited
+    public List<AbstractTask> execute(List<AbstractTask> wholeTaskList) {
+	if (wholeTaskList == null || wholeTaskList.size() <= 0)
+	    throw new IllegalArgumentException(
+		    "taskList cannot be empty or null");
+	
 	this.wholeTaskList = wholeTaskList;
-	oldTask = editSpace.get(index);
+	originalTask = editSpace.get(index - 1);
+	editedTask = editTask(originalTask);
+	
+	wholeTaskList.remove(originalTask);
+	wholeTaskList.add(editedTask);	
 
-	StringTokenizer editParameterTokenizer = new StringTokenizer(
-		editParameter, ".");
+	return generateReturnList(originalTask, editedTask);
+    }
 
+    // edits a task according to the parameter given and returns it
+    private AbstractTask editTask(AbstractTask originalTask) {
+	AbstractTask editedTask = (AbstractTask) originalTask.clone();
+	
+	StringTokenizer parameterTokenizer = new StringTokenizer(editParameter, ".", true);
+	
+	String paramToken = parameterTokenizer.nextToken();
+	if(!paramToken.equals(".")) editedTask.setDescription(paramToken);
+	
+	return editedTask;
+    }
+
+    // undoes this edit operation
+    // returns the list: edited->original
+    public List<AbstractTask> undo() {
+	// the task list from where the edited task came from must still exist
+	assert wholeTaskList != null; 
+	// the edited task must exist in task list
+	assert wholeTaskList.contains(editedTask); 
+	
+	wholeTaskList.remove(editedTask);
+	wholeTaskList.add(originalTask);
+
+	return generateReturnList(editedTask, originalTask);
+    }
+
+    // generates the returned task list
+    private List<AbstractTask> generateReturnList(AbstractTask oldTask, AbstractTask newTask){
 	List<AbstractTask> returnList = new Vector<AbstractTask>();
 	returnList.add(oldTask);
-
+	returnList.add(newTask);
+	
 	return returnList;
     }
-
-    public List<AbstractTask> undo() {
-	assert wholeTaskList != null; // the task list from where the edited
-				      // task came from must still exist
-	assert oldTask != null; // the edited task must still exist
-
-	return null;
-    }
-
 }
