ab60ea8415e28178a38401327aaa9fe3dc29cea1
ShawnPamela@localhost <ShawnPamela@localhost>
2012-10-11 10:39:40 +0800

    added return empty vector

--- src/Clear.java
+++ src/Clear.java
@@ -2,16 +2,25 @@ import java.util.List;
 import java.util.Vector;
 
 public class Clear implements UndoableCommand{
-	
-	private List<AbstractTask> undoList=new Vector<AbstractTask>();
-	
-	public List<AbstractTask> execute(List<AbstractTask> taskList){
+
+	private List<AbstractTask> wholeTaskList;
+
+	public Clear(){
+	}
+
+	public List<AbstractTask> execute(List<AbstractTask> wholeTaskList){
+		return generateReturnList();
+	}
+
+
+	private List<AbstractTask> generateReturnList() {
 		List<AbstractTask> returnList = new Vector<AbstractTask>();
 		return returnList;
 	}
-	
+
 	public List<AbstractTask> undo(){
-		return undoList;
+		
+		//return undoList;
 	}
-	
+
 }
