ef09f23ee3d1ed1caf8a6c4ab84a25ca2ec18741
ChingSK@lenovo-y450.gateway.2wire.net <ChingSK@lenovo-y450.gateway.2wire.net>
2012-10-22 08:08:38 +0800

    Remove unneeded import and indentation

--- src/Clear.java
+++ src/Clear.java
@@ -1,18 +1,17 @@
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Vector;
 
 public class Clear implements UndoableCommand {
 
 	private List<AbstractTask> wholeTaskList;
-	private List<AbstractTask> undoTaskList=new LinkedList<AbstractTask>();
+	private List<AbstractTask> undoTaskList = new LinkedList<AbstractTask>();
 
 	public Clear() {
 	}
 
 	public List<AbstractTask> execute(List<AbstractTask> wholeTaskList) {
 		this.wholeTaskList = wholeTaskList;
-		for(int i=0;i<wholeTaskList.size();i++){
+		for (int i = 0; i < wholeTaskList.size(); i++) {
 			undoTaskList.add(wholeTaskList.get(i));
 		}
 		wholeTaskList.clear();
@@ -20,7 +19,7 @@ public class Clear implements UndoableCommand {
 	}
 
 	public List<AbstractTask> undo() {
-		for(int i=0;i<undoTaskList.size();i++){
+		for (int i = 0; i < undoTaskList.size(); i++) {
 			wholeTaskList.add(undoTaskList.get(i));
 		}
 		return null;
