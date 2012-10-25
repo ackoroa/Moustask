10550a28c84030934417dcc2ae71702d9cd6e232
ShawnPamela@localhost <ShawnPamela@localhost>
2012-10-25 09:52:51 +0800

    added sample logging for clear

--- src/Clear.java
+++ src/Clear.java
@@ -1,21 +1,37 @@
+import java.io.IOException;
 import java.util.LinkedList;
+import java.util.logging.*;
 import java.util.List;
 
 public class Clear implements UndoableCommand {
 
 	private List<AbstractTask> wholeTaskList;
 	private List<AbstractTask> undoTaskList = new LinkedList<AbstractTask>();
-
+	private static Logger clearLogger = Logger.getLogger("clearLog");
+	
+	
 	public Clear() {
 	}
 
 	public List<AbstractTask> execute(List<AbstractTask> wholeTaskList) {
+		try{
+		FileHandler fileHandler=new FileHandler("clearLogging.txt",true);
+		clearLogger.addHandler(fileHandler);
+		
 		this.wholeTaskList = wholeTaskList;
+		
 		for (int i = 0; i < wholeTaskList.size(); i++) {
 			undoTaskList.add(wholeTaskList.get(i));
+			clearLogger.log(Level.INFO, i+" loop/s");
 		}
+		clearLogger.log(Level.INFO, "No exception");
 		wholeTaskList.clear();
 		return null;
+		}		
+		catch (IOException ex){
+			clearLogger.log(Level.WARNING, "Exception",ex);
+			return null;
+		}
 	}
 
 	public List<AbstractTask> undo() {
