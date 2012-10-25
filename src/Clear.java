96964eb2c237f6467b9e0a38b7fab10806625638
ShawnPamela@localhost <ShawnPamela@localhost>
2012-10-25 12:05:52 +0800

    Logging for clear class

--- src/Clear.java
+++ src/Clear.java
@@ -8,35 +8,40 @@ public class Clear implements UndoableCommand {
 	private List<AbstractTask> wholeTaskList;
 	private List<AbstractTask> undoTaskList = new LinkedList<AbstractTask>();
 	private static Logger clearLogger = Logger.getLogger("clearLog");
-	
-	
+
 	public Clear() {
 	}
 
 	public List<AbstractTask> execute(List<AbstractTask> wholeTaskList) {
-		try{
-		FileHandler fileHandler=new FileHandler("clearLogging.txt",true);
-		clearLogger.addHandler(fileHandler);
-		
-		this.wholeTaskList = wholeTaskList;
-		
-		for (int i = 0; i < wholeTaskList.size(); i++) {
-			undoTaskList.add(wholeTaskList.get(i));
-			clearLogger.log(Level.INFO, i+" loop/s");
+		try {
+			FileHandler fileHandler = new FileHandler("clearLogging.txt", true);
+			clearLogger.addHandler(fileHandler);
+			this.wholeTaskList = wholeTaskList;
+			for (int i = 0; i < wholeTaskList.size(); i++) {
+				undoTaskList.add(wholeTaskList.get(i));
+				clearLogger.log(Level.INFO, wholeTaskList.get(i)
+						.getDescription() + " task/s cleared.");
+			}
+			clearLogger.log(Level.INFO, "Undo task list duplicated.");
+			wholeTaskList.clear();
+		} catch (IOException ex) {
+			clearLogger.log(Level.WARNING, "Exception thrown.", ex);
 		}
-		clearLogger.log(Level.INFO, "No exception");
-		wholeTaskList.clear();
 		return null;
-		}		
-		catch (IOException ex){
-			clearLogger.log(Level.WARNING, "Exception",ex);
-			return null;
-		}
 	}
 
 	public List<AbstractTask> undo() {
-		for (int i = 0; i < undoTaskList.size(); i++) {
-			wholeTaskList.add(undoTaskList.get(i));
+		try {
+			FileHandler fileHandler = new FileHandler("clearLogging.txt", true);
+			clearLogger.addHandler(fileHandler);
+			for (int i = 0; i < undoTaskList.size(); i++) {
+				wholeTaskList.add(undoTaskList.get(i));
+				clearLogger.log(Level.INFO, wholeTaskList.get(i)
+						.getDescription() + " task/s undo.");
+			}
+			clearLogger.log(Level.INFO, "Original task list reverted.");
+		} catch (IOException ex) {
+			clearLogger.log(Level.WARNING, "Exception thrown.", ex);
 		}
 		return null;
 	}
