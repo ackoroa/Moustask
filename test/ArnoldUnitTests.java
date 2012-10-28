a3653432e1a916dc0002d73741e3fc372ec65b01
Arnold <arnold.koroa@gmail.com>
2012-10-28 10:20:10 +0800

    change name of all test

--- test/ArnoldUnitTests.java
+++ test/ArnoldUnitTests.java
@@ -5,6 +5,6 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ EditTest.class, DeadlineTaskTest.class, DeleteTest.class,
 	FloatingTaskTest.class, TimedTaskTest.class })
-public class AllTests {
+public class ArnoldUnitTests {
 
 }
