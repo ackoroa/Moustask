c1c5293b77b87afbec2f745b92d788e5fc198a71
ShawnPamela@localhost <ShawnPamela@localhost>
2012-10-28 20:48:45 +0800

    added Junit test for clear class

---
+++ test/ClearTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import java.util.Vector;
+import org.junit.Test;
+
+public class ClearTest {
+	
+	public static Vector<AbstractTask> taskList;
+	@Before
+	public void setUpBeforeClass() throws Exception {
+		taskList = new Vector<AbstractTask>();
+		taskList.add(new FloatingTask("I write words", "Home"));
+
+		taskList.add(new DeadlineTask("Do project", "2012-12-12 18:00"));
+
+		taskList.add(new TimedTask("Project Got Time", "2012-12-12 18:00",
+				"2012-12-12 20:00", "School"));
+	}
+
+	@Test
+	public void test() {
+		Clear clearTest = new Clear();
+		clearTest.execute(taskList);
+		assertEquals(taskList.size(), 0);
+		
+		clearTest.undo();
+		System.out.println(taskList);
+		assertEquals(taskList.size(),3);
+	}
+	
+
+}
