4342b8d678674ee81b6ff23571cb125dc464f817
Ching Sieh Kae <siehkae@gmail.com>
2012-11-04 10:00:57 +0800

    Improve welcome message display

--- src/MousTask.java
+++ src/MousTask.java
@@ -16,9 +16,9 @@ class MousTask {
 	}
 
 	private void showWelcomeMessage() {
-		System.out.println("Welcome to MousTask.");
+		System.out.println("Welcome to MousTask!");
 		System.out.println("Type .help to view the user guide.");
 		System.out.println("Use the search or display command first before edit or delete any tasks.");
-		System.out.println("Type .exit to exit the program.\n");
+		System.out.println("Type .exit to exit the program.");
 	}
 }
