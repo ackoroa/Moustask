d59aea9768e649b44e6ba0042206fbb0885f3f7d
Ching Sieh Kae <siehkae@gmail.com>
2012-11-07 06:21:42 +0800

    add logictest

--- test/AllUnitTests.java
+++ test/AllUnitTests.java
@@ -4,10 +4,10 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AddTest.class, ClearTest.class, DeadlineTaskTest.class,
-	DeleteTest.class, EditTest.class, FloatingTaskTest.class,
-	HelpTest.class, SearchTest.class, StorageTest.class,
-	TimedTaskTest.class, TypeTaskPairTest.class })
+@SuiteClasses({ LogicTest.class, AddTest.class, ClearTest.class,
+		DeadlineTaskTest.class, DeleteTest.class, EditTest.class,
+		FloatingTaskTest.class, HelpTest.class, SearchTest.class,
+		StorageTest.class, TimedTaskTest.class, TypeTaskPairTest.class })
 public class AllUnitTests {
 
 }
